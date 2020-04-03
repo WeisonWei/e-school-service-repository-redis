534f7c999e397f2bb4b025c8b75736cbcb7b85ed
WeisonWei <weixx3@126.com>
2020-04-03 19:45:18 +0800

    init

---
+++ src/main/java/com/es/redis/ESchoolServiceRepositoryRedisApplication.java
@@ -0,0 +1,13 @@
+package com.es.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESchoolServiceRepositoryRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ESchoolServiceRepositoryRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/es/redis/ESchoolServiceRepositoryRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESchoolServiceRepositoryRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
